b37bdfc46e921d655aa1df2c2b5b51bede911ba5
akhilmohan123 <akhilmohan8547@gmail.com>
2025-05-20 21:55:39 +0530

    started implemented the ffmpeg module for converting blob and pipe to rtmp

--- socket/index.js
+++ socket/index.js
@@ -1,3 +1,5 @@
+const {spawn}=require("child_process");
+let ffmpegProcess=null
 const io=require("socket.io")(8800,{
     cors:{
         orgin:"http://localhost:3001",
@@ -6,6 +8,7 @@ const io=require("socket.io")(8800,{
 let activeusers=[]
 
 io.on("connection",(socket)=>{
+  let ffmpegstatus=false
     //add new user
     socket.on("new-user-add",(newUserId)=>{
         //if user is not added previously
@@ -25,9 +28,34 @@ io.on("connection",(socket)=>{
       io.to(user.socketId).emit("recieve-message", data);
     }
   });
-  socket.on("video-data",(data)=>{
-    console.log(data);
-  })
+  socket.on("video-data", (data) => {
+    if (!ffmpegProcess) {
+      ffmpegProcess = spawn('ffmpeg', [
+        '-i', 'pipe:0',
+        '-c:v', 'libx264',
+        '-preset', 'ultrafast',
+        '-tune', 'zerolatency',
+        '-f', 'flv',
+        'rtmp://localhost/live/stream' // or your public RTMP endpoint
+      ]);
+
+      ffmpegProcess.stderr.on('data', (data) => {
+        console.log('FFmpeg stderr:', data.toString());
+      });
+
+      ffmpegProcess.on('close', (code) => {
+        console.log(`FFmpeg exited with code ${code}`);
+        ffmpegProcess = null;
+      });
+
+      console.log("FFmpeg process started.");
+    }
+
+    if (ffmpegProcess) {
+      const buffer = Buffer.from(new Uint8Array(data)); // Convert ArrayBuffer to Buffer
+      ffmpegProcess.stdin.write(buffer);
+    }
+  });
     socket.on("disconnect",()=>{
         activeusers=activeusers.filter((user)=>user.socketId!==socket.id);
         console.log("user disconnected",activeusers)
